94c5dfe6bb7ee5cfd4d5d2da314579d85c500ddc
Ines <ines_gbadamassi@outlook.fr>
2024-05-21 21:44:12 +0200

    Update chat part

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,29 +12,46 @@ const Main = () => {
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="" />
            </div>
+
            <div className="main-container">
-            <div className="greet">
-                <p><span>Hello, Dev.</span></p>
-                <p>How can I help you today?</p>
+
+            {!showResult
+            ?<>
+                <div className="greet">
+                    <p><span>Hello, Dev.</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readibility of the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                </div>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
+            </>
+            : <div className='result'>
+                <div className='result-title'>
+                    <img src={assets.user_icon_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                <div/>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" className="" />
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
                 </div>
-                <div className="card">
-                    <p>Briefly summarize this concept: urban planning</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Brainstorm team bonding activities for our work retreat</p>
-                    <img src={assets.message_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Improve the readibility of the following code</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
-            </div>
+            <div/>
+            }
+
 
             <div className="main-bottom">
                 <div className="search-box">
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -17,6 +17,7 @@ const ContextProvider = (props) => {
         setResultData("")
         setLoading(true)
         setShowResult(true)
+        setRecentPrompt(input)
         const response = await runChat(input)
         setResultData(response)
         setLoading(false)
