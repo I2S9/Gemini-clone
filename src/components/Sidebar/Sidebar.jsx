5d97143232bac3658e999a7f411729ee3568b296
Ines <ines_gbadamassi@outlook.fr>
2024-05-21 22:28:01 +0200

    Fix previous prompts (chat)

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { Context } from '../../context/Context'
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+  const {onSent, prevPrompts,setRecentPrompt} = useContext(Context)
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt)
+    await onSent(prompt)
+  }
 
   return (
     <div className="sidebar">
@@ -16,10 +23,15 @@ const Sidebar = () => {
         {extended ? (
           <div className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is react...</p>
-            </div>
+            {prevPrompts.map((item,index)=>{
+              return (
+                <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              )
+            })}
+
           </div>
         ) : null}
       </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,14 +12,49 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = userState(false);
     const [resultData, setResultData] = useState("");
 
+    const delayPara = (index, nextWord) => {
+        setTimeout(function (){
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
+        let response;
+        if (prompt !== undefined){
+            response = await runChat(prompt);
+            setRecentPrompt(prompt)
+        }
+        else{
+            setPrevPrompts(prev =>[...prev, input])
+            setRecentPrompt(input)
+            response = await runChat(input)
+        }
+        
         setRecentPrompt(input)
+        setPrevPrompts(prev=>[...prev,input])
         const response = await runChat(input)
-        setResultData(response)
+        let responseArray = response.split("**");
+        let newResponse;
+        for(let i=0; i<responseArray.length; i++)
+        {
+            if (i === 0 || i%2 !== 1){
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>" + responseArray[i]+"<b/>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        let newResponseArray = newResponse2.split("");
+        for(let i=0; i<newResponseArray.length;i++)
+        {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord+" ")
+        }
         setLoading(false)
         setInput("")
     }
